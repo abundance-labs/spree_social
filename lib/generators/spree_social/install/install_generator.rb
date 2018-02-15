989903a186c0fe4ccdefd3ce1eedc7765bd7fde7
Artem Kozaev <artemkozaev@gmail.com>
2018-02-15 15:09:31 +0300

    Fixes install generator to include frontend js

--- lib/generators/spree_social/install/install_generator.rb
+++ lib/generators/spree_social/install/install_generator.rb
@@ -3,8 +3,12 @@ module SpreeSocial
     class InstallGenerator < Rails::Generators::Base
       class_option :auto_run_migrations, type: :boolean, default: false
 
+      def add_javascripts
+        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_social\n"
+      end
+
       def add_stylesheets
-        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_social\n", before: /\*\//, verbose: true
+        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_social\n"
       end
 
       def add_migrations
